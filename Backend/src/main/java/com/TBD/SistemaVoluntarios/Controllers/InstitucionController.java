241b04b527f287a198454791eff1586c14a138af
PedroPlazaT <pedro.plaza@usach.cl>
2023-10-22 06:04:47 -0300

    [Pedro Plaza] Se termina CRUD de Institucion.

--- Backend/src/main/java/com/TBD/SistemaVoluntarios/Controllers/InstitucionController.java
+++ Backend/src/main/java/com/TBD/SistemaVoluntarios/Controllers/InstitucionController.java
@@ -1,14 +1,13 @@
 package com.TBD.SistemaVoluntarios.Controllers;
 
+import com.TBD.SistemaVoluntarios.Entities.HabilidadEntity;
 import com.TBD.SistemaVoluntarios.Entities.InstitucionEntity;
 import com.TBD.SistemaVoluntarios.Repositories.InstitucionRepository;
 import com.TBD.SistemaVoluntarios.RepositoriesImplement.InstitucionRepoImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,10 +22,35 @@ public class InstitucionController {
         this.institucionRepository = InstitucionRepository;
     }
 
+    // CREATE: crear una nueva institucion.
+    @PostMapping("/nueva-institucion")
+    public ResponseEntity<String> createInstitucion(@RequestBody InstitucionEntity institucion)
+    {
+        institucionRepository.createInstitucion(institucion);
+        return ResponseEntity.ok("Institucion creada con exito");
+    }
+
+    // READ: Listar todas las instituciones.
     @GetMapping("/lista-institucion")
     public ResponseEntity<List<InstitucionEntity>> listar(Model model)
     {
         return ResponseEntity.ok(institucionRepository.findAll());
+    }
+    // UPDATE: Actualiza la descripcion de una institucion.
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateDesc(@PathVariable Integer id,
+                                             @RequestParam("descrip") String descrip)
+    {
+        institucionRepository.updateDescrip(id, descrip);
+        return ResponseEntity.ok("Descripcion de la institucion actualizada con exito");
+    }
+
+    // DELETE: Elimina una institucion.
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteEm(@PathVariable Integer id)
+    {
+        institucionRepository.deleteById(id);
+        return ResponseEntity.ok("Institucion eliminada exitosamente");
     }
 
 }
