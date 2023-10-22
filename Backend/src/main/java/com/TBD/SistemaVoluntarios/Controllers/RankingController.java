a59ca9f011940d4bb421d4dacf6e37032813e7ee
PedroPlazaT <pedro.plaza@usach.cl>
2023-10-21 22:51:53 -0300

    [Pedro Plaza] Se crea Controller y RepoImp de Ranking

--- Backend/src/main/java/com/TBD/SistemaVoluntarios/Controllers/RankingController.java
+++ Backend/src/main/java/com/TBD/SistemaVoluntarios/Controllers/RankingController.java
@@ -2,29 +2,63 @@ package com.TBD.SistemaVoluntarios.Controllers;
 
 import com.TBD.SistemaVoluntarios.Entities.RankingEntity;
 import com.TBD.SistemaVoluntarios.Repositories.RankingRepository;
+import com.TBD.SistemaVoluntarios.RepositoriesImplement.RankingRepoImp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/rank")
+@RequestMapping("/api/rankings")
 public class RankingController {
-    private final RankingRepository rankingRepository;
-
-    RankingController(RankingRepository rankingRepository){
-        this.rankingRepository = rankingRepository;
+    private final RankingRepoImp rankingRepoImp;
+    @Autowired
+    public RankingController(RankingRepoImp rankingRepoImpepoimp)
+    {
+        this.rankingRepoImp = rankingRepoImpepoimp;
     }
 
-
-    @GetMapping("/lista")
-    public ResponseEntity<List<RankingEntity>> listar(Model model)
+    // CREATE: crear un nuevo ranking.
+    @PostMapping("/agregar-ranking")
+    public ResponseEntity<String> crearRanking(RankingEntity ranking)
     {
-        return ResponseEntity.ok(rankingRepository.findAll());
+        rankingRepoImp.createRanking(ranking);
+        return ResponseEntity.ok("Ranking creado con exito");
+    }
+
+    // READ: leer todos los rankings.
+    @GetMapping("/lista-rankings")
+    public ResponseEntity<List<RankingEntity>> leerTodoElRanking()
+    {
+        List<RankingEntity> rankings = rankingRepoImp.leerTodoElRanking();
+        return ResponseEntity.ok(rankings);
+    }
+
+    // READ: leer un ranking por ID.
+    @GetMapping("/{id}")
+    public ResponseEntity<RankingEntity> leerRankingPorId(Integer id)
+    {
+        RankingEntity ranking = rankingRepoImp.leerRankingPorId(id);
+        return ResponseEntity.ok(ranking);
+    }
+
+    // UPDATE: actualiza el puntaje de un ranking, en base a un ID.
+    @PutMapping("/id")
+    public ResponseEntity<String> actualizarPuntaje(Integer id, @RequestParam("nuevoPuntaje") Integer nuevoPuntaje)
+    {
+        rankingRepoImp.actualizarPuntaje(id, nuevoPuntaje);
+        return ResponseEntity.ok("Puntaje actualizado con exito.");
+    }
+
+    // DELETE: eliminar un ranking por ID.
+    @DeleteMapping("/id")
+    public ResponseEntity<String> eliminarRankingPorId(Integer id)
+    {
+        rankingRepoImp.eliminarRankingPorId(id);
+        return ResponseEntity.ok("Ranking eliminado exitosamente.");
     }
 
 }
--- Backend/src/main/java/com/TBD/SistemaVoluntarios/RepositoriesImplement/RankingRepoImp.java
+++ Backend/src/main/java/com/TBD/SistemaVoluntarios/RepositoriesImplement/RankingRepoImp.java
@@ -3,6 +3,7 @@ package com.TBD.SistemaVoluntarios.RepositoriesImplement;
 import com.TBD.SistemaVoluntarios.Entities.RankingEntity;
 import com.TBD.SistemaVoluntarios.Repositories.RankingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
@@ -10,19 +11,67 @@ import org.sql2o.Sql2o;
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
-public class RankingRepoImp implements RankingRepository{
-    @Autowired
-    Sql2o sql2o;
+@Repository
+public class RankingRepoImp {
 
-    @Override
-    public List<RankingEntity> findAll() {
-        try(Connection conn = sql2o.open()){
-            return conn.createQuery("select * from Ranking order by id")
-                    .executeAndFetch(RankingEntity.class);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return null;
+    private final Sql2o sql2o;
+
+    public RankingRepoImp(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    // CREATE: crear un nuevo ranking
+    public void createRanking(RankingEntity ranking)
+    {
+        try (Connection con = sql2o.open()) {
+            String sql = "INSERT INTO ranking (ID_VOLUNTARIO, ID_TAREA, puntaje, flg_invitado, flg_participa) " +
+                    "VALUES (:voluntarioId, :tareaId, :puntaje, :flgInvitado, :flgParticipa)";
+            con.createQuery(sql)
+                    .addParameter("voluntarioId", ranking.getID_VOLUNTARIO())
+                    .addParameter("tareaId", ranking.getID_TAREA())
+                    .addParameter("puntaje", ranking.getPuntaje())
+                    .addParameter("flgInvitado", ranking.getFlg_invitado())
+                    .addParameter("flgParticipa", ranking.getFlg_participa())
+                    .executeUpdate();
+        }
+    }
+
+    // READ: Obtener todos los rankings
+    public List<RankingEntity> leerTodoElRanking() {
+        try (Connection con = sql2o.open()) {
+            String sql = "SELECT * FROM ranking";
+            return con.createQuery(sql).executeAndFetch(RankingEntity.class);
+        }
+    }
+
+    // READ: leer ranking por ID
+    public RankingEntity leerRankingPorId(Integer id) {
+        try (Connection con = sql2o.open()) {
+            String sql = "SELECT * FROM ranking WHERE ID_RANKING = :id";
+            return con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(RankingEntity.class);
+        }
+    }
+
+    // UPDATE: actualiza el puntaje de un ranking, en base a un ID.
+    public void actualizarPuntaje(Integer id, Integer nuevoPuntaje) {
+        try (Connection con = sql2o.open()) {
+            String sql = "UPDATE ranking SET puntaje = :nuevoPuntaje WHERE ID_RANKING = :id";
+            con.createQuery(sql)
+                    .addParameter("nuevoPuntaje", nuevoPuntaje)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+    }
+
+    // DELETE: elimina un ranking por ID
+    public void eliminarRankingPorId(Integer id) {
+        try (Connection con = sql2o.open()) {
+            String sql = "DELETE FROM ranking WHERE ID_RANKING = :id";
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
         }
     }
 }
