ecc456c1f6686f71e9554ea9bbe89ebc569a1c19
Jehison Bustamante <jehison.bustamante@usach.cl>
2023-10-22 06:35:19 -0300

    [Jehison Bustamante] CRUD Tabla intermedia VOLHAB

--- Backend/src/main/java/com/TBD/SistemaVoluntarios/Repositories/VolHabilidadRepository.java
+++ Backend/src/main/java/com/TBD/SistemaVoluntarios/Repositories/VolHabilidadRepository.java
@@ -10,6 +10,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public interface VolHabilidadRepository{
+    //CREATE: Crear un nuevo intermedio
+    void createVolHab(VolHabilidadEntity volHabilidad);
+
     List<VolHabilidadEntity> findAll();
 
+    //READ: Buscar una relacion a partir de el id de voluntario
+    VolHabilidadEntity findByID_VOLUNTARIO(Integer id);
+
+    //READ: Buscar una relacion a partir de el id de habilidad
+    VolHabilidadEntity findByID_HABILIDAD(Integer id);
+
+    //UPDATE: Actualiza el id de voluntario
+    void updateID_VOLUNTARIO(Integer id, Integer nuevoIdVol);
+
+    //UPDATE: Actualiza el id de habilidad
+    void updateID_HABILIDAD(Integer id, Integer nuevoIDHab);
+
+    // DELETE: elimina un ranking por ID
+    void deleteById(Integer id);
 }
--- Backend/src/main/java/com/TBD/SistemaVoluntarios/RepositoriesImplement/VolHabilidadRepoImp.java
+++ Backend/src/main/java/com/TBD/SistemaVoluntarios/RepositoriesImplement/VolHabilidadRepoImp.java
@@ -4,11 +4,9 @@ import com.TBD.SistemaVoluntarios.Entities.VolHabilidadEntity;
 import com.TBD.SistemaVoluntarios.Repositories.VolHabilidadRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -16,7 +14,21 @@ public class VolHabilidadRepoImp implements VolHabilidadRepository {
     @Autowired
     Sql2o sql2o;
 
+    //CREATE: Crear un nuevo intermedio
+    @Override
+    public void createVolHab(VolHabilidadEntity volHabilidad)
+    {
+        try (Connection con = sql2o.open()) {
+            String sql = "INSERT INTO vol_habilidad (ID_VOLUNTARIO, ID_HABILIDAD) " +
+                    "VALUES (:id_voluntario, :id_habilidad)";
+            con.createQuery(sql)
+                    .addParameter("voluntario", volHabilidad.getID_VOLUNTARIO())
+                    .addParameter("habilidad", volHabilidad.getID_HABILIDAD())
+                    .executeUpdate();
+        }
+    }
 
+    //READ: Buscar todos los intermedios
     @Override
     public List<VolHabilidadEntity> findAll() {
         try(Connection conn = sql2o.open()){
@@ -25,6 +37,69 @@ public class VolHabilidadRepoImp implements VolHabilidadRepository {
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return null;
+        }
+    }
+
+    //READ: Buscar una relacion a partir de el id de voluntario
+    @Override
+    public VolHabilidadEntity findByID_VOLUNTARIO(Integer id) {
+        try (Connection conn = sql2o.open()) {
+            List<VolHabilidadEntity> volHabilidadEntities = conn.createQuery("select * from vol_habilidad where ID_VOLUNTARIO=:id")
+                    .addParameter("ID_VOLUNTARIO", id)
+                    .executeAndFetch(VolHabilidadEntity.class);
+            return volHabilidadEntities.get(0);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+    //READ: Buscar una relacion a partir de el id de habilidad
+    @Override
+    public VolHabilidadEntity findByID_HABILIDAD(Integer id) {
+        try (Connection conn = sql2o.open()) {
+            List<VolHabilidadEntity> volHabilidadEntities = conn.createQuery("select * from vol_habilidad where ID_HABILIDAD=:id")
+                    .addParameter("ID_HABILIDAD", id)
+                    .executeAndFetch(VolHabilidadEntity.class);
+            return volHabilidadEntities.get(0);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    //UPDATE: Actualiza el id de voluntario
+    @Override
+    public void updateID_VOLUNTARIO(Integer id, Integer nuevoIdVol) {
+        try (Connection con = sql2o.open()) {
+            String sql = "UPDATE vol_habilidad SET ID_VOLUNTARIO = :nuevoIdVol WHERE ID_VOL_HAB = :id";
+            con.createQuery(sql)
+                    .addParameter("nuevoIdVol", nuevoIdVol)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+    }
+    //UPDATE: Actualiza el id de habilidad
+    @Override
+    public void updateID_HABILIDAD(Integer id, Integer nuevoIDHab) {
+        try (Connection con = sql2o.open()) {
+            String sql = "UPDATE vol_hablidad SET ID_HABILIDAD = :nuevoIdHab WHERE ID_HABILIDAD = :id";
+            con.createQuery(sql)
+                    .addParameter("nuevoIDHab", nuevoIDHab)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+    }
+
+    // DELETE: elimina una relacion por ID
+    @Override
+    public void deleteById(Integer id) {
+        try (Connection con = sql2o.open()) {
+            String sql = "DELETE FROM vol_habilidad WHERE ID_VOL_HAB = :id";
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
